10dc4930cb7cb1802c7978cb9be2bbc97f43bf75
Darik A O'Neil <doneil@salk.edu>
2026-01-16 11:35:27 -0800

    Modified for modern python packaging/build standards/misc. Only utilizing multiple population dynamics, rest untested. Speed up ~ 2.4x, unclear how it scales

--- setup.py
+++ setup.py
@@ -2,64 +2,26 @@
 import os
 import numpy as np
 from Cython.Build import cythonize
+from setuptools import setup
 from setuptools.extension import Extension
-from setuptools import setup, find_packages
 
-# Make sources available using relative paths from this file's directory.
+# Ensure builds run relative to the project root
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# Create the extensions. Manually enumerate the required
-# Only compile with OpenMP if user asks for it
-USE_OPENMP = os.environ.get('USE_OPENMP', False)
+USE_OPENMP = os.environ.get("USE_OPENMP", "").lower() in ("1", "true", "yes")
 print("USE_OPENMP", USE_OPENMP)
-extensions = []
-extensions.append(
-    Extension('ssm.cstats',
-              extra_compile_args=["-fopenmp"] if USE_OPENMP else [],
-              extra_link_args=["-fopenmp"] if USE_OPENMP else [],
-              language="c++",
-              sources=["ssm/cstats.pyx"],
-              include_dirs=[np.get_include()]
-              )
-)
 
-DISTNAME = 'ssm'
-DESCRIPTION = 'Bayesian learning and inference for a variety of state space models'
-with open('README.md') as fp:
-    LONG_DESCRIPTION = fp.read()
-MAINTAINER = 'Scott Linderman'
-MAINTAINER_EMAIL = 'scott.linderman@stanford.edu'
-URL = ''
-LICENSE = 'MIT'
-DOWNLOAD_URL = ''
-VERSION = '0.0.1'
+extensions = [
+    Extension(
+        "ssm.cstats",
+        sources=["ssm/cstats.pyx"],
+        language="c++",
+        include_dirs=[np.get_include()],
+        extra_compile_args=["-fopenmp"] if USE_OPENMP else [],
+        extra_link_args=["-fopenmp"] if USE_OPENMP else [],
+    )
+]
 
-
-if __name__ == "__main__":
-    setup(name=DISTNAME,
-          maintainer=MAINTAINER,
-          maintainer_email=MAINTAINER_EMAIL,
-          description=DESCRIPTION,
-          license=LICENSE,
-          url=URL,
-          version=VERSION,
-          download_url=DOWNLOAD_URL,
-          long_description=LONG_DESCRIPTION,
-          zip_safe=False,  # the package can run out of an .egg file
-          classifiers=[
-              'Intended Audience :: Science/Research',
-              'Intended Audience :: Developers',
-              'License :: OSI Approved',
-              'Programming Language :: C',
-              'Programming Language :: Python',
-              'Topic :: Software Development',
-              'Topic :: Scientific/Engineering',
-              'Operating System :: Microsoft :: Windows',
-              'Operating System :: POSIX',
-              'Operating System :: Unix',
-              'Operating System :: MacOS',
-              'Programming Language :: Python :: 3.7',
-          ],
-          packages=find_packages(),
-          ext_modules = cythonize(extensions)
-          )
+# metadata in pyproject.toml will be used by setuptools.build_meta;
+# we only provide ext_modules here
+setup(ext_modules=cythonize(extensions))
